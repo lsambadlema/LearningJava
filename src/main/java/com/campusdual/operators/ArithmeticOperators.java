6f042d6539896ebe55f3fd3222cf5a85cbf525e5
Lorena Sambad Lema <lsambadlema@gmail.com>
2024-10-09 16:58:27 +0200

    Ejercicio de Logica de operadores

---
+++ src/main/java/com/campusdual/operators/ArithmeticOperators.java
@@ -0,0 +1,27 @@
+package com.campusdual.operators;
+
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+
+        int value = 5;                 // 5
+        value = value + 3;              //8
+        value = 2 - value;              //-6
+        value += 3;                     //-3
+        value -= 1;                     //-4
+        value *= 4;                     //-16
+        value = (value - 2) / 2;        //-9
+        int newValue = 3;               //3
+        int myValue = 2;                //2
+        value = newValue + myValue;     //5
+        final int CONST = 2;            //2
+        value = CONST;                  //2
+        value +=3;                      //5
+        value = value % 2;              //1
+        value++;                        //2
+        value--;                        //1
+        System.out.println(value);      //1
+
+
+    }
+}
+
---
+++ src/main/java/com/campusdual/operators/LogicOperators.java
@@ -0,0 +1,14 @@
+package com.campusdual.operators;
+
+public class LogicOperators {
+    public static void main(String[] args) {
+        System.out.println("4 is equal to 4:" + (4==4));
+        System.out.println("3 is different from 4:" + (3!=4));
+        System.out.println("3 is greater from 5:" + (3>5));
+        System.out.println("5 is less or equal from 5:" + (5<=5));
+        System.out.println("Unique true combination for AND: " + (true && true));
+        System.out.println("Unique false combination for OR: " + (false || false));
+        System.out.println("NOT false: " + (!false));
+
+    }
+}
